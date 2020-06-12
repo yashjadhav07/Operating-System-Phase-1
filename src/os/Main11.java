fb5f668a95810893365731026a583374a4d94336
Yash Jadhav <yash.jadhav18@vit.edu>
2020-06-12 10:08:13 +0530

    Operating System Phase 1

---
+++ src/os/Main11.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package os;
+
+/**
+ *
+ * @author kiran
+ */
+public class Main11 {
+
+    public static void main(String[] args) {
+	// write your code here
+        OS_Yash yash =new OS_Yash("input_latest.txt","output.txt");
+        yash.LOAD();
+        //OS_Abhijit abhi =new OS_Abhijit("Abhijit_Jobs.txt","Abhijit_output.txt");
+        //abhi.LOAD();
+        //OS_Om om =new OS_Om("Om_Jobs.txt","Om_output.txt");
+        //om.LOAD();
+        //OS_Onkar onkar =new OS_Onkar("Onkar_Jobs.txt","Onkar_output.txt");
+        //onkar.LOAD();
+        
+    }
+}
